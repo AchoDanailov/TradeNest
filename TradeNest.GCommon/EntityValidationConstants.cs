dfcb3fbe1d9928a147da81f82db373753b72e920
A. Danailov <danailov.angel.99@gmail.com>
2026-02-02 00:45:57 +0200

    Add models and dtos validation constant values

---
+++ TradeNest.GCommon/EntityValidationConstants.cs
@@ -0,0 +1,20 @@
+namespace TradeNest.GCommon;
+
+/// <summary>
+/// This class holds constant values used to validate the correct state of models and DTO's across all layers of the application.
+/// </summary>
+public static class EntityValidationConstants
+{
+    public static class Product
+    {
+        public const byte NameMinLengthValue = 3;
+        public const byte NameMaxLengthValue = 255;
+
+        public const short UrlMaxLengthValue = 2048;
+
+        public const byte DescriptionMinLengthValue = 5;
+        public const short DescriptionMaxLengthValue = 3000;
+
+        public const string SellingPriceColumnDataType = "DECIMAL(10, 2)";
+    }
+}
