070fa6e4d9f52ec7b2ba6185377126992a81be3c
A. Danailov <danailov.angel.99@gmail.com>
2026-02-10 21:40:36 +0200

    Impl ProductsController's Index, BestSellers, Details actions

--- TradeNest.Web/Controllers/ProductsController.cs
+++ TradeNest.Web/Controllers/ProductsController.cs
@@ -1,8 +1,10 @@
+using TradeNest.Data;
+using TradeNest.Data.Models;
+using TradeNest.Web.ViewModels;
+using static TradeNest.GCommon.ApplicationConstants;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
-using TradeNest.Data;
-using TradeNest.Web.ViewModels;
 
 namespace TradeNest.Web.Controllers;
 
@@ -10,78 +12,111 @@ namespace TradeNest.Web.Controllers;
 public class ProductsController : Controller
 {
     private TradeNestDbContext _dbContext;
-
+    private IEnumerable<string> _categoriesNames;
+        
     public ProductsController(TradeNestDbContext dbContext)
     {
         this._dbContext = dbContext;
+        this._categoriesNames = this.GetCategoriesNames();
     }
 
     [HttpGet]
     [AllowAnonymous]
-    public IActionResult All(
+    public IActionResult Index(
         [FromQuery] string? category = null,
-        [FromQuery] string? search = null, 
-        [FromQuery] string? orderBy = null)
+        [FromQuery] string? search = null)
     {
-        if (!string.IsNullOrWhiteSpace(search))
-        {
-            return Json(search); 
-        }
-        
-        if (!string.IsNullOrEmpty(category))
-        {
-            return Json(category);
-        }
-        
-        if (!string.IsNullOrWhiteSpace(orderBy))
-        {
-            return Json(orderBy); 
-        }
-        
-        CategoriesAndProductsViewModel categoriesAndProductsViewModel
-            = new CategoriesAndProductsViewModel()
+        CatalogProductsAndCategoriesViewModel viewModel
+            = new CatalogProductsAndCategoriesViewModel()
             {
-                Products = this._dbContext.Products
-                    .AsNoTracking()
-                    .OrderByDescending(p => p.CreatedOn)
-                    .Select(p => new ProductViewModel()
-                    {
-                        Id = p.Id,
-                        Name = p.Name,
-                        SellingPrice = p.SellingPrice.ToString("F2"),
-                        FrontImageUrl = p.Images
-                            .FirstOrDefault(i => i.IsFrontImage == true)!
-                            .Url,
-                        CategoryName = p.Category.Name,
-                    }),
-                AllCategories = this._dbContext.Categories
-                    .AsNoTracking()
-                    .OrderBy(c => c.Name)
-                    .Select(c => new CategoryViewModel()
-                    {
-                        Id = c.Id,
-                        Name = c.Name
-                    })
+                CategoriesNames = this._categoriesNames,
             };
         
-        return View(categoriesAndProductsViewModel);
+        IQueryable<Product> products = this._dbContext.Products
+            .AsNoTracking();
+        if (!string.IsNullOrWhiteSpace(search))
+        {
+            viewModel.IsSearchResultSet = true;
+            products = products
+                .Where(p => p.Name.Contains(search));
+        }
+
+        bool isValidCategory = viewModel.CategoriesNames
+            .Any(categoryName => categoryName == category);
+        if (string.IsNullOrEmpty(category) || isValidCategory)
+        {
+            return NotFound();
+        }
+        
+        products = products
+            .Include(p => p.Category)
+            .Where(p => p.Category.Name == category);
+
+        IEnumerable<ProductViewModel> productsViewModels = products
+            .OrderByDescending(p => p.CreatedOn)
+            .Select(p => new ProductViewModel()
+            {
+                Id = p.Id,
+                Name = p.Name,
+                SellingPrice = p.SellingPrice.ToString(PricesFormat),
+                FrontImageUrl = p.Images
+                    .SingleOrDefault(i => i.IsFrontImage == true)!
+                    .Url,
+                CategoryName = p.Category.Name,
+            })
+            .ToArray();
+
+        viewModel.Products = productsViewModels;
+        
+        return View(viewModel);
     }
 
     [HttpGet]
     [AllowAnonymous]
-    public IActionResult Details([FromRoute] string? id)
+    public IActionResult BestSellers()
     {
-        if (string.IsNullOrEmpty(id) || !Guid.TryParse(id, out Guid guidId))
+        CatalogProductsAndCategoriesViewModel viewModel
+            = new CatalogProductsAndCategoriesViewModel()
+            {
+                CategoriesNames = this._categoriesNames,
+                IsSearchResultSet = false,
+            };
+        
+        IEnumerable<ProductViewModel> products = this._dbContext.Products
+            .AsNoTracking()
+            .OrderByDescending(p => p.CreatedOn)
+            .Select(p => new ProductViewModel()
+            {
+                Id = p.Id,
+                Name = p.Name,
+                SellingPrice = p.SellingPrice.ToString(PricesFormat),
+                FrontImageUrl = p.Images
+                    .SingleOrDefault(i => i.IsFrontImage == true)!
+                    .Url,
+                CategoryName = p.Category.Name,
+            })
+            .ToArray();
+
+        viewModel.Products = products;
+        
+        return View(nameof(Index), viewModel);
+    }
+
+    [HttpGet]
+    [AllowAnonymous]
+    public IActionResult Details([FromRoute] string? id = null)
+    {
+        if (string.IsNullOrEmpty(id) || !Guid.TryParse(id, out Guid guidIdValue))
         {
             return BadRequest();
         }
 
-        var product = this._dbContext.Products
+        Product? product = this._dbContext.Products
             .Include(p => p.Owner)
             .Include(p => p.Category)
             .Include(p => p.Images)
             .AsNoTracking()
-            .FirstOrDefault(p => p.Id == guidId);
+            .FirstOrDefault(p => p.Id == guidIdValue);
         if (product == null)
         {
             return NotFound();
@@ -93,17 +128,26 @@ public class ProductsController : Controller
             Name = product.Name,
             Description = product.Description,
             QuantityInStock = product.QuantityInStock,
-            SellingPrice = product.SellingPrice.ToString("f2"),
+            SellingPrice = product.SellingPrice.ToString(PricesFormat),
             IsEnabled = product.IsEnabled,
-            Owner = product.Owner!.UserName ?? string.Empty,
+            Owner = product.Owner?.UserName ?? string.Empty,
             CategoryName = product.Category.Name,
             FrontImageUrl = product.Images
-                .FirstOrDefault(i => i.IsFrontImage == true)!
+                .SingleOrDefault(i => i.IsFrontImage)!
                 .Url,
             ImagesUrls = product.Images
                 .Select(i => i.Url),
         };
         
         return View(productDetailsViewModel);
+    }
+
+    private IEnumerable<string> GetCategoriesNames()
+    {
+        return this._dbContext.Categories
+            .AsNoTracking()
+            .OrderBy(c => c.Name)
+            .Select(c => c.Name)
+            .ToArray();
     }
 }
