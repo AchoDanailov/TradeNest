f13df77cf7c55ea335cd3532007bb767f11f091e
A. Danailov <danailov.angel.99@gmail.com>
2026-02-11 15:20:38 +0200

    Add initial state for the product create form model

--- TradeNest.Web.ViewModels/Category/AllCategoriesViewModel.cs
+++ TradeNest.Web.ViewModels/Category/AllCategoriesViewModel.cs
@@ -2,5 +2,7 @@ namespace TradeNest.Web.ViewModels.Category;
 
 public class AllCategoriesViewModel
 {
-    
+    public Guid Id { get; set; }
+
+    public string CategoryName { get; set; } = null!;
 }
--- TradeNest.Web.ViewModels/Product/ProductCreateFormModel.cs
+++ TradeNest.Web.ViewModels/Product/ProductCreateFormModel.cs
@@ -1,6 +1,46 @@
+using System.ComponentModel.DataAnnotations;
+using TradeNest.Web.ViewModels.Category;
+using static TradeNest.GCommon.EntityValidationConstants.Product;
+using static TradeNest.GCommon.EntityValidationConstants.Image;
+using static TradeNest.GCommon.EntityValidationConstants.User;
+
 namespace TradeNest.Web.ViewModels.Product;
 
 public class ProductCreateFormModel
 {
+    [Required]
+    [StringLength(NameMaxLengthValue, MinimumLength = NameMinLengthValue)]
+    public string ProductName { get; set; } = null!;
+
+    [Required]
+    [StringLength(DescriptionMaxLengthValue, MinimumLength = DescriptionMinLengthValue)]
+    public string Description { get; set; } = null!;
+
+    [Required]
+    [Range(MinQuantityInStockValue, MaxQuantityInStockValue)]
+    public int QuantityInStock { get; set; }
     
+    [Required]
+    [Range(minimum: (double)MinSellingPriceValue, maximum: Double.MaxValue)]
+    public decimal SellingPrice { get; set; }
+    
+    public decimal? CostPrice { get; set; }
+    
+    [Required]
+    public bool IsEnabled { get; set; }
+    
+    [Required]
+    [StringLength(UserNameMaxValidLengthValue, MinimumLength = UserNameMinValidLengthValue)]
+    public string OwnerName { get; set; } = null!;
+
+    [Required]
+    [StringLength(UrlMaxLengthValue, MinimumLength = UrlMinLengthValue)]
+    [DataType(DataType.ImageUrl)]
+    public string FrontImageUrl { get; set; } = null!;
+
+    [Required]
+    public string CategoryId { get; set; } = null!;
+
+    public IEnumerable<AllCategoriesViewModel> AllCategoriesForSelectInputFieldOptions { get; set; }
+        = new List<AllCategoriesViewModel>();
 }
