b3207be4a299b2191fd89ff40da734952270edd9
A. Danailov <danailov.angel.99@gmail.com>
2026-02-14 14:21:29 +0200

    Add comment to the OperationsStatusMessages class explaining its ussage

--- TradeNest.Web/Utilities/OperationsStatusMessages.cs
+++ TradeNest.Web/Utilities/OperationsStatusMessages.cs
@@ -1,5 +1,9 @@
 namespace TradeNest.Web.Utilities;
 
+/// <summary>
+/// This class holds status messages used to notify the user about the status of attempted operations
+/// available in the application.
+/// </summary>
 public static class OperationsStatusMessages
 {
     public const string ProductCreationUnexpectedErrorMessage 
