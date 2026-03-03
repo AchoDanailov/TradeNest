33a7b608820552639d823425a93f5f503e6a2a5e
A. Danailov <danailov.angel.99@gmail.com>
2026-03-03 10:16:02 +0200

    Make the StatusNotificationMessages class and members internal. (justification: no need for them to be public) + fix the query string produced when clicking on category on Products/Index.

--- TradeNest.Web/Utilities/StatusNotificationMessages.cs
+++ TradeNest.Web/Utilities/StatusNotificationMessages.cs
@@ -4,23 +4,23 @@ namespace TradeNest.Web.Utilities;
 /// This class holds status messages used to notify the user about the status of attempted operations
 /// available in the application.
 /// </summary>
-public static class StatusNotificationMessages
+internal static class StatusNotificationMessages
 {
-    public const string ProductCreationUnexpectedErrorMessage 
+    internal const string ProductCreationUnexpectedErrorMessage 
         = "Oops. Something went wrong while trying to add your new product. Please try again in a moment.";
     
-    public const string ProductModificationUnexpectedErrorMessage 
+    internal const string ProductModificationUnexpectedErrorMessage 
         = "Oops. Something went wrong while trying save your changes. Please try again in a moment.";
 
-    public const string ProductDeletionSuccessMessage 
+    internal const string ProductDeletionSuccessMessage 
         = "The product was successfully deleted.";
 
-    public const string OrderModificationUnexpectedErrorMessage
+    internal const string OrderModificationUnexpectedErrorMessage
         = "Oops. Something went wrong with the last attempt to modify your order. Please try again in a moment.";
 
-    public const string OrderSubmittionSuccessMessage
+    internal const string OrderSubmittionSuccessMessage
         = "Your order was successfully submitted.";
 
-    public const string UnexpectedErrorMessage
+    internal const string UnexpectedErrorMessage
         = "Oops. Something went wrong. Please try again in a moment.";
 }
