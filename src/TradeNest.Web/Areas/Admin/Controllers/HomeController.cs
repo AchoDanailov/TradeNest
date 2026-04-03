a3c3bda1983cb70af5a3ac37bae1c755b0907fbb
A. Danailov <danailov.angel.99@gmail.com>
2026-04-03 14:24:54 +0300

    Add Admin/Home/Index view.

--- src/TradeNest.Web/Areas/Admin/Controllers/HomeController.cs
+++ src/TradeNest.Web/Areas/Admin/Controllers/HomeController.cs
@@ -5,7 +5,7 @@ namespace TradeNest.Web.Areas.Admin.Controllers;
 public class HomeController : BaseAdminController 
 {
     [HttpGet]
-    [Route("/Admin/Home/Index")] [Route("/Admin/Home")] [Route("/Admin/Dashboard")]
+    [Route("/Admin/Home/Index")] [Route("/Admin/Home")] [Route("/Admin/ControlPanel")]
     public async Task<IActionResult> Index()
     {
         return View();
